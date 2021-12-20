7c92d4b1436fdda4d483e46e1300590f31b6c442
hui.wang <hui.wang@kyligence.io>
2021-12-20 16:38:44 +0800

    MDX-3896, Upgrade log4j2 to 2.17.0

--- build/deploy.sh
+++ build/deploy.sh
@@ -4,10 +4,10 @@ cd "$( dirname "$0" )"
 cd ..
 
 current_dir=`pwd`
-echo "current_dir : $current_dir"
+echo "current directory : $current_dir"
 
 # VERSION, need to update per version.
-version="gateway-2.0.1.1000"
+version="gateway-2.0.2.1002"
 echo ${version}"."`date +%s`  > ${current_dir}/VERSION
 
 mvn clean package -P km -DskipTests -Dmaven.javadoc.skip=true
