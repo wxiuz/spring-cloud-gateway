22f8ccf809777515f613dcc6492ba8d550c18d61
yong.fu <yong.fu@kyligence.io>
2022-03-12 01:21:16 +0800

    KM-3450,fix error message

--- build/deploy.sh
+++ build/deploy.sh
@@ -7,7 +7,7 @@ current_dir=`pwd`
 echo "current_dir : $current_dir"
 
 # VERSION, need to update per version.
-version="gateway-2.0.1.1000"
+version="gateway-3.1.1.1000"
 echo ${version}"."`date +%s`  > ${current_dir}/VERSION
 
 mvn clean package -P km -DskipTests -Dmaven.javadoc.skip=true
