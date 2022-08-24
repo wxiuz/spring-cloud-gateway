06321453c64f27cdd6e520cc224c9ed850a8b4bd
yong.fu <yong.fu@kyligence.io>
2022-08-24 17:50:14 +0800

    upgrade version to 3.1.3

--- build/deploy.sh
+++ build/deploy.sh
@@ -7,7 +7,7 @@ current_dir=`pwd`
 echo "current_dir : $current_dir"
 
 # VERSION, need to update per version.
-version="gateway-3.1.1.1000"
+version="gateway-3.1.3.1000"
 echo ${version}"."`date +%s`  > ${current_dir}/VERSION
 
 mvn clean package -P km -DskipTests -Dmaven.javadoc.skip=true
